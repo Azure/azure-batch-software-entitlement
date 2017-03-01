b92cc9ed25af4cfe86a5dd5714b44e483fb2c9c1
Bevan Arps <bevan.arps@microsoft.com>
2017-03-02 12:15:23 +1300

    Add missing fullstops

--- src/sescmd/ServerOptions.cs
+++ src/sescmd/ServerOptions.cs
@@ -8,14 +8,14 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement
     [Verb("server", HelpText = "Run as a standalone software entitlement server.")]
     public class ServerOptions
     {
-        [Option('s', "signature", HelpText = "Certificate thumbprint of the certificate used to sign the token")]
+        [Option('s', "signature", HelpText = "Certificate thumbprint of the certificate used to sign the token.")]
         public string SignatureThumbprint { get; set; }
 
         //TODO: Document where this looks to find the certificate (in a cross platform way)
-        [Option('e', "encrypt", HelpText = "Certificate thumbprint of the certificate used to encrypt the token")]
+        [Option('e', "encrypt", HelpText = "Certificate thumbprint of the certificate used to encrypt the token.")]
         public string EncryptionThumbprint { get; set; }
 
-        [Option('x', "exit-after-request", HelpText = "Request the server exits after serving one request")]
+        [Option('x', "exit-after-request", HelpText = "Request the server exits after serving one request.")]
         public bool ExitAfterRequest { get; set; }
     }
 }
