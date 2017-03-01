62301e68043132b50e2a9436e221c7b265144e6b
Bevan Arps <bevan.arps@outlook.com>
2017-03-02 12:21:53 +1300

    Initial prototype for sescmd shell (#1)
    * Solution and empty sescmd project
    
    * Prototype for sescmd shells
    
    * Change namespace
    
    * Removed use of the phrase "application package"
    
    * Fix capitalization of Azure
    
    * Rename property to not-after
    
    * Add //TODO to document where certificates come from
    
    * Add token-file option
    
    * Use "Azure Batch" instead of "batch"
    
    * Improve docs
    
    * Improve naming - and note where better names are needed
    
    * Code gardening
    
    * Remove option not needed
    
    * Use entitlement-id instead of application-id
    
    * Specify time zone and format for timestamp parameters
    
    * Update documentation and types for GenerateOptions
    
    * Code formatting
    
    * Rename verb to "server"
    
    * Add missing fullstops
    
    * Fix typos

---
+++ src/sescmd/GenerateOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using CommandLine;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement
+{
+    [Verb("generate", HelpText = "Generate a token with specified parameters")]
+    public class GenerateOptions
+    {
+        [Option("entitlement-id", HelpText = "Unique identifier(s) for the entitlement(s) to include (comma separated).", Separator = ',')]
+        public IEnumerable<string> EntitlementIds { get; set; }
+
+        [Option("vmid", HelpText = "Unique identifier for the Azure virtual machine")]
+        public string VirtualMachineId { get; set; }
+
+        [Option("not-before",
+            HelpText = "The moment at which the token becomes active and the application is entitled to execute (format 'hh:mm d-mmm-yyyy'; 24 hour clock; local time; defaults to now).")]
+        public string NotBefore { get; set; }
+
+        [Option("not-after", HelpText = "The moment at which the token expires and the application is no longer entitled to execute (format 'hh:mm d-mmm/-yyyy'; 24 hour clock; local time; defaults to 7 days).")]
+        public string NotAfter { get; set; }
+
+        [Option("address", HelpText = "The externally visible IP address of the machine entitled to execute the application.")]
+        public IPAddress Address { get; set; }
+
+        //TODO: Document where this looks to find the certificate (in a cross platform way)
+        [Option('s', "sign", HelpText = "Certificate thumbprint of the certificate used to sign the token.")]
+        public string SignatureThumbprint { get; set; }
+
+        //TODO: Document where this looks to find the certificate (in a cross platform way)
+        [Option("encrypt", HelpText = "Certificate thumbprint of the certificate used to encrypt the token.")]
+        public string EncryptionThumbprint { get; set; }
+
+        [Option('f', "--token-file", HelpText = "The name of a file into which the token will be written (token will be written to stdout otherwise).")]
+        public string TokenFile { get; set; }
+
+    }
+}
---
+++ src/sescmd/Program.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            return Parser.Default
+                .ParseArguments<GenerateOptions, VerifyOptions, ServerOptions>(args)
+                .MapResult(
+                    (GenerateOptions options) => Generate(options),
+                    (VerifyOptions options) => Verify(options),
+                    (ServerOptions options) => Serve(options),
+                    errors => 1);
+        }
+
+        public static int Generate(GenerateOptions options)
+        {
+            return 0;
+        }
+
+        public static int Verify(VerifyOptions options)
+        {
+            return 0;
+        }
+
+        public static int Serve(ServerOptions options)
+        {
+            return 0;
+        }
+    }
+}
