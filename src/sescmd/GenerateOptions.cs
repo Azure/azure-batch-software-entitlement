820afc4048e210de2dc3bbd725117f7c0257a314
Bevan Arps <bevan.arps@microsoft.com>
2017-03-02 14:01:10 +1300

    Introduce base class for options

--- src/sescmd/GenerateOptions.cs
+++ src/sescmd/GenerateOptions.cs
@@ -6,7 +6,7 @@ using CommandLine;
 namespace Microsoft.Azure.Batch.SoftwareEntitlement
 {
     [Verb("generate", HelpText = "Generate a token with specified parameters")]
-    public class GenerateOptions
+    public class GenerateOptions : OptionsBase
     {
         [Option("entitlement-id", HelpText = "Unique identifier(s) for the entitlement(s) to include (comma separated).", Separator = ',')]
         public IEnumerable<string> EntitlementIds { get; set; }
---
+++ src/sescmd/OptionsBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement
+{
+    public class OptionsBase
+    {
+        [Option("quiet", HelpText = "Suppress most logging output (errors and warnings will still show).")]
+        public bool Quiet { get; set; }
+
+        [Option("verbose", HelpText = "Show verbose logging.")]
+        public bool Verbose { get; set; }
+
+        [Option("debug", HelpText = "Show debug logging as well (maximum information).")]
+        public bool Debug { get; set; }
+    }
+}
