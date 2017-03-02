f821d8aebc4c799f803e5492b32d5c63327d5207
Bevan Arps <bevan.arps@microsoft.com>
2017-03-02 15:39:15 +1300

    Fix merge issues

--- src/sescmd/Program.cs
+++ src/sescmd/Program.cs
@@ -35,5 +35,62 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement
             var logger = CreateLogger(options);
             return 0;
         }
+
+        public static ILogger CreateLogger(OptionsBase options)
+        {
+            var level = SelectLogEventLevel(options);
+            var logger = CreateLogger(level);
+            WarnAboutInactiveOptions(options, level, logger);
+            return logger;
+        }
+
+        private static ILogger CreateLogger(LogEventLevel level)
+        {
+            var result = new LoggerConfiguration()
+                            .WriteTo.LiterateConsole()
+                            .MinimumLevel.Is(level)
+                            .CreateLogger();
+
+            result.Information("Software Entitlement Service Command Line Utility");
+            return result;
+        }
+
+        public static LogEventLevel SelectLogEventLevel(OptionsBase options)
+        {
+            if (options.Debug)
+            {
+                return LogEventLevel.Debug;
+            }
+
+            if (options.Verbose)
+            {
+                return LogEventLevel.Verbose;
+            }
+
+            if (options.Quiet)
+            {
+                return LogEventLevel.Warning;
+            }
+
+            return LogEventLevel.Information;
+        }
+
+        private static void WarnAboutInactiveOptions(OptionsBase options, LogEventLevel level, ILogger logger)
+        {
+            if (options.Quiet && level < LogEventLevel.Warning)
+            {
+                logger.Warning("Logging at {Level}; ignoring --quiet", level);
+            }
+
+            if (options.Debug && level > LogEventLevel.Debug)
+            {
+                logger.Warning("Logging at {Level}; ignoring --debug", level);
+            }
+
+            if (options.Verbose && level > LogEventLevel.Verbose)
+            {
+                logger.Warning("Logging at {Level}; ignoring --verbose", level);
+            }
+        }
     }
 }
