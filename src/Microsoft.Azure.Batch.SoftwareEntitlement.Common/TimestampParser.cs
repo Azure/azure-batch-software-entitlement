10dfa4876a9dc0f26f65c91d29b89ef7171d05ec
Bevan Arps <bevan.arps@microsoft.com>
2017-03-03 15:10:17 +1300

    Create TimestampParser with tests

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/TimestampParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using Serilog;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
+{
+    /// <summary>
+    /// Utility class for conversion of dates from strings
+    /// </summary>
+    /// <remarks>Wraps our specific conventions and knows about logging for diagnostics.</remarks>
+    public class TimestampParser
+    {
+        // The expected format for timestamps
+        public const string ExpectedFormat = "HH:mm d-MMM-yyyy";
+
+        // Reference to our common logger
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="TimestampParser"/> class
+        /// </summary>
+        /// <param name="logger">Logger to use for any warnings.</param>
+        public TimestampParser(ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Option<DateTimeOffset> Parse(string value)
+        {
+            DateTimeOffset result;
+            if (DateTimeOffset.TryParseExact(
+                value, ExpectedFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out result))
+            {
+                // Correctly parsed in the expected format
+                return Option<DateTimeOffset>.Some(result);
+            }
+
+            if (DateTimeOffset.TryParse(
+                value, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out result))
+            {
+                // Correctly parsed with detected format
+                _logger.Warning(
+                    "Timestamp {TimeStamp} was not in the expected format {Format}; using {DateTime:F}.", 
+                    value, 
+                    ExpectedFormat, 
+                    result);
+                return Option<DateTimeOffset>.Some(result);
+            }
+
+            return Option<DateTimeOffset>.None();
+        }
+    }
+}
---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/TimestampParserTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using Serilog;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public class TimestampParserTests
+    {
+        // Substitute logger used for testing
+        private readonly ILogger _logger = Substitute.For<ILogger>();
+
+        public class Constructor : TimestampParserTests
+        {
+            [Fact]
+            public void GivenNullLogger_ThrowsException()
+            {
+                Assert.Throws<ArgumentNullException>(
+                    () => new TimestampParser(null));
+            }
+        }
+
+        public class ParseMethod : TimestampParserTests
+        {
+            // Initialized parser to use for testing
+            private readonly TimestampParser _parser;
+
+            // Timestamp for testing
+            private readonly DateTimeOffset _timestamp;
+
+            public ParseMethod()
+            {
+                _parser = new TimestampParser(_logger);
+                _timestamp = DateTimeOffset.Now.At(14, 39); // Needs to have 0 seconds and 0 milliseconds
+            }
+
+            [Fact]
+            public void GivenEmptyString_ReturnsNone()
+            {
+                var result = _parser.Parse(string.Empty);
+                result.HasValue.Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenTimestampInExpectedFormat_ReturnsExpectedValue()
+            {
+                var valueToParse = _timestamp.ToString(TimestampParser.ExpectedFormat);
+                var result = _parser.Parse(valueToParse);
+                result.Apply(
+                    whenSome: d => d.Should().Be(_timestamp),
+                    whenNone: () => Assert.True(false, "Expect to have a value"));
+            }
+
+            [Fact]
+            public void GivenTimestampInDifferentFormat_ReturnsExpectedValue()
+            {
+                var valueToParse = _timestamp.ToString("G");
+                var result = _parser.Parse(valueToParse);
+                result.Apply(
+                    whenSome: d => d.Should().Be(_timestamp),
+                    whenNone: () => Assert.True(false, "Expect to have a value"));
+            }
+
+            [Fact]
+            public void GivenTimestampInDifferentFormat_GeneratesWarning()
+            {
+                var valueToParse = _timestamp.ToString("G");
+                _parser.Parse(valueToParse);
+
+                // Slightly nasty because we need to match argument types to select the right overload
+                _logger.Received().Warning(
+                    Arg.Any<string>(),
+                    Arg.Any<string>(),
+                    Arg.Any<string>(),
+                    Arg.Any<DateTimeOffset>());
+            }
+        }
+    }
+}
