4fab74a6cfc857cec380e5688de5c6efbdb57c4c
Bevan Arps <bevan.arps@outlook.com>
2017-03-07 10:22:20 +1300

    Abstractions for logging (#3)
    * Create ISimpleLogger abstraction
    * Create Serilog implementation of ISimpleLogger
    * Create MonitoringLogger and tests
    * Modify command line to use new logger

---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/MonitoringLoggerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public class MonitoringLoggerTests
+    {
+        // Discarding fake logger for testing
+        private readonly ISimpleLogger _nulLogger;
+
+        // Monitoring Logger for testing
+        private readonly MonitoringLogger _monitoringLogger;
+
+        public MonitoringLoggerTests()
+        {
+            _nulLogger = new NullLogger();
+            _monitoringLogger = new MonitoringLogger(_nulLogger);
+        }
+
+        public class HaveLoggedErrors : MonitoringLoggerTests
+        {
+            [Fact]
+            public void WhenNoErrorsLogged_ReturnsFalse()
+            {
+                _monitoringLogger.HasErrors.Should().BeFalse();
+            }
+
+            [Fact]
+            public void AfterErrorsLogged_ReturnsTrue()
+            {
+                _monitoringLogger.Error("Message");
+                _monitoringLogger.HasErrors.Should().BeTrue();
+            }
+        }
+
+        public class HaveLoggedWarnings : MonitoringLoggerTests
+        {
+            [Fact]
+            public void WhenNoWarningsLogged_ReturnsFalse()
+            {
+                _monitoringLogger.HasWarnings.Should().BeFalse();
+            }
+
+            [Fact]
+            public void AfterWarningsLogged_ReturnsTrue()
+            {
+                _monitoringLogger.Warning("Message");
+                _monitoringLogger.HasWarnings.Should().BeTrue();
+            }
+        }
+    }
+}
