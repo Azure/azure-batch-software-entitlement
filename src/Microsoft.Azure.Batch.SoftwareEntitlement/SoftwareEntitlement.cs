04a26472108cdc2b649e266c96be22fb9b835c44
Bevan Arps <bevan.arps@microsoft.com>
2017-03-06 15:32:25 +1300

    Create SoftwareEntitlement class

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement/SoftwareEntitlement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement
+{
+    /// <summary>
+    /// Definition of a software entitlement
+    /// </summary>
+    public class SoftwareEntitlement
+    {
+        /// <summary>
+        /// Gets the virtual machine identifier for the machine entitled to use the specified packages
+        /// </summary>
+        public string VirtualMachineId { get; }
+
+        /// <summary>
+        /// The earliest moment at which the entitlement is active
+        /// </summary>
+        public DateTimeOffset NotBefore { get; }
+
+        /// <summary>
+        /// The latest moment at which the entitlement is active
+        /// </summary>
+        public DateTimeOffset NotAfter { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoftwareEntitlement"/> class
+        /// </summary>
+        /// <param name="virtualMachineId">Unique identifier of the virtual machine.</param>
+        /// <param name="notBefore">Earliest instant the entitlement is valid.</param>
+        /// <param name="notAfter">Latest instance the entitlement is valid.</param>
+        public SoftwareEntitlement(string virtualMachineId, DateTimeOffset notBefore, DateTimeOffset notAfter)
+        {
+            VirtualMachineId = virtualMachineId;
+            NotBefore = notBefore;
+            NotAfter = notAfter;
+        }
+    }
+}
