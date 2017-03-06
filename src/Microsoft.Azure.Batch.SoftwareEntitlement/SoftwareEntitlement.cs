7e203c82215863e51cd0b350a094ff2cdfe146e2
Bevan Arps <bevan.arps@microsoft.com>
2017-03-06 17:12:39 +1300

    Handle timestamp parsing for tokens

--- src/Microsoft.Azure.Batch.SoftwareEntitlement/SoftwareEntitlement.cs
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement/SoftwareEntitlement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Azure.Batch.SoftwareEntitlement.Common;
 
 namespace Microsoft.Azure.Batch.SoftwareEntitlement
 {
@@ -9,32 +10,144 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement
     /// </summary>
     public class SoftwareEntitlement
     {
+        // Reference to the logger we use for reporting activity
+        private readonly MonitoringLogger _logger;
+
+        // Parser used to convert from text into instants of time
+        private readonly TimestampParser _timestampParser;
+
         /// <summary>
         /// Gets the virtual machine identifier for the machine entitled to use the specified packages
         /// </summary>
-        public string VirtualMachineId { get; }
+        public string VirtualMachineId { get; private set; }
+
+        /// <summary>
+        /// The moment at which the entitlement was created
+        /// </summary>
+        public DateTimeOffset Created { get; private set; }
 
         /// <summary>
         /// The earliest moment at which the entitlement is active
         /// </summary>
-        public DateTimeOffset NotBefore { get; }
+        public DateTimeOffset NotBefore { get; private set; }
 
         /// <summary>
         /// The latest moment at which the entitlement is active
         /// </summary>
-        public DateTimeOffset NotAfter { get; }
+        public DateTimeOffset NotAfter { get; private set; }
+
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SoftwareEntitlement"/> class
         /// </summary>
-        /// <param name="virtualMachineId">Unique identifier of the virtual machine.</param>
-        /// <param name="notBefore">Earliest instant the entitlement is valid.</param>
-        /// <param name="notAfter">Latest instance the entitlement is valid.</param>
-        public SoftwareEntitlement(string virtualMachineId, DateTimeOffset notBefore, DateTimeOffset notAfter)
+        public SoftwareEntitlement(ISimpleLogger logger)
         {
-            VirtualMachineId = virtualMachineId;
-            NotBefore = notBefore;
-            NotAfter = notAfter;
+            var now = DateTimeOffset.Now;
+
+            _logger = new MonitoringLogger(logger);
+            _timestampParser = new TimestampParser(_logger);
+
+            VirtualMachineId = string.Empty;
+            Created = now;
+            NotBefore = now;
+            NotAfter = now + TimeSpan.FromDays(7);
+
+            _logger.Debug("Token creation {Timestamp:d/mmm/yyyy HH:MM}", now);
+        }
+
+        /// <summary>
+        /// Specify the virtual machine Id of the machine 
+        /// </summary>
+        /// <param name="virtualMachineId"></param>
+        /// <returns></returns>
+        public SoftwareEntitlement WithVirtualMachineId(string virtualMachineId)
+        {
+            if (string.IsNullOrWhiteSpace(virtualMachineId))
+            {
+                _logger.Error("Virtual Machine ID must be specified");
+                return this;
+            }
+
+            _logger.Debug("Virtual machine id is {VirtualMachineId}", virtualMachineId);
+
+            return new SoftwareEntitlement(this)
+            {
+                VirtualMachineId = virtualMachineId
+            };
+        }
+
+        /// <summary>
+        /// Specify the time frame for which the token will be valid
+        /// </summary>
+        /// <param name="notBefore">Earliest instant of availability.</param>
+        /// <param name="notAfter">Latest instant of availability.</param>
+        /// <returns></returns>
+        public SoftwareEntitlement ForTimespan(string notBefore, string notAfter)
+        {
+            DateTimeOffset start;
+            if (!_timestampParser.TryParse(notBefore, out start))
+            {
+                start = NotBefore;
+                _logger.Warning("No valid token start specified; using {NotBefore}", start);
+            }
+
+            DateTimeOffset finish;
+            if (!_timestampParser.TryParse(notAfter, out finish))
+            {
+                finish = NotAfter;
+                _logger.Warning("No valid token expiry specified; using {NotAfter}", finish);
+            }
+
+            return ForTimespan(start, finish);
+        }
+
+
+        /// <summary>
+        /// Specify the time frame for which the token will be valid
+        /// </summary>
+        /// <param name="notBefore">Earliest instant of availability.</param>
+        /// <param name="notAfter">Latest instant of availability.</param>
+        /// <returns></returns>
+        public SoftwareEntitlement ForTimespan(DateTimeOffset notBefore, DateTimeOffset notAfter)
+        {
+            if (notAfter < notBefore)
+            {
+                _logger.Error("Token expiry of {NotAfter} must be after token becomes active at {NotBefore}.", notAfter, notBefore);
+                return this;
+            }
+
+            if (notAfter < DateTimeOffset.Now)
+            {
+                _logger.Warning("Token expiry of {NotAfter} has already passed", notAfter);
+            }
+
+            if (notBefore > DateTimeOffset.Now)
+            {
+                _logger.Information("Token does not become available until {NotBefore}", notBefore);
+            }
+
+            var result = new SoftwareEntitlement(this)
+            {
+                NotBefore = notBefore,
+                NotAfter = notAfter
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoftwareEntitlement"/> class
+        /// </summary>
+        /// <param name="original">Original entitlement to clone.</param>
+        public SoftwareEntitlement(SoftwareEntitlement original)
+        {
+            _logger = original._logger;
+            _timestampParser = original._timestampParser;
+
+            VirtualMachineId = original.VirtualMachineId;
+            Created = original.Created;
+            NotBefore = original.NotBefore;
+            NotAfter = original.NotAfter;
         }
     }
 }
