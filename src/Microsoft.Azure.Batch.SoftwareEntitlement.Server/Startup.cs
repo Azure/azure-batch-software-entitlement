ad3d8849e39ffb21ce35c659497d5632a22ac723
Bevan Arps <bevan.arps@microsoft.com>
2017-03-08 13:27:52 +1300

    Serve static files from standalone server

--- src/Microsoft.Azure.Batch.SoftwareEntitlement.Server/Startup.cs
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Server/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Batch.SoftwareEntitlement.Common;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Microsoft.Azure.Batch.SoftwareEntitlement.Server
 {
@@ -16,7 +14,7 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Server
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
@@ -34,10 +32,12 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddSerilog(SimpleLoggerFactory.Serilogger);
 
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.UseMvc(); // TODO: Turn this on later
         }
     }
 }
--- src/sescmd/Program.cs
+++ src/sescmd/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using CommandLine;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Azure.Batch.SoftwareEntitlement.Common;
 using Microsoft.Azure.Batch.SoftwareEntitlement.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
 
@@ -31,34 +36,49 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement
                 Console.ReadLine();
             }
 
-            return result;
+            return exitCode;
+        }
+
+        private static void SetupLogging(OptionsBase options)
+        {
+            var level = options.SelectLogEventLevel();
+            var logger = SimpleLoggerFactory.CreateLogger(level);
+            logger.Information("Software Entitlement Service Command Line Utility");
+            options.WarnAboutInactiveOptions(level, logger);
         }
 
         public static int Generate(GenerateOptions options)
         {
-
             return 0;
         }
 
         public static int Verify(VerifyOptions options)
         {
+            return 0;
+        }
 
+        public static int Serve(ServerOptions options)
+        {
+            var contentDirectory = FindContentDirectory();
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseContentRoot(contentDirectory.FullName)
                 .UseStartup<Startup>()
                 .Build();
 
+            // This sends output directly to the console which is a bit naff
+            // but avoiding it would probably be brittle.
             host.Run();
 
             return 0;
         }
 
+        private static DirectoryInfo FindContentDirectory()
         {
-
-            {
-            }
-
+            var hostAssembly = typeof(Startup).GetTypeInfo().Assembly;
+            var hostFileInfo = new FileInfo(hostAssembly.Location);
+            var hostDirectory = hostFileInfo.Directory;
+            return hostDirectory;
         }
     }
 }
