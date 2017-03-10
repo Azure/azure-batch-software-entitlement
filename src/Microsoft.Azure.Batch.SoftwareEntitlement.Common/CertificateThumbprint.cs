6a0d7c64eacd2b6cd698207181235c2217a3d538
Bevan Arps <bevan.arps@microsoft.com>
2017-03-10 15:41:02 +1300

    Create CertificateThumbprint class

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/CertificateThumbprint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
+{
+    /// <summary>
+    /// Represents the thumbprint of a certificate as a semantic type
+    /// </summary>
+    public sealed class CertificateThumbprint : IEquatable<CertificateThumbprint>
+    {
+        // The actual thumbprint we wrap
+        private readonly string _thumbprint;
+
+        /// <summary>
+        /// Initializes a new instance of the CertificateThumbprint class
+        /// </summary>
+        /// <param name="thumbprint">Thumbprint to wrap</param>
+        public CertificateThumbprint(string thumbprint)
+        {
+            _thumbprint = SanitizeThumbprint(thumbprint);
+        }
+
+        /// <summary>
+        /// Is this thumbprint equal to another thumbprint
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(CertificateThumbprint other)
+        {
+            if (other == null)
+            {
+                // Not equal to null
+                return false;
+            }
+
+            return string.Equals(_thumbprint, other._thumbprint, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Does this thumbprint match the thumbprint from the specified string?
+        /// </summary>
+        /// <remarks>Formatting of the thumbprints (e.g. whitespace) is ignored.</remarks>
+        /// <param name="thumbprint">String to compare</param>
+        /// <returns>True if the thumbprints match, false otherwise.</returns>
+        public bool HasThumbprint(string thumbprint)
+        {
+            var t = SanitizeThumbprint(thumbprint ?? string.Empty);
+            return string.Equals(_thumbprint, t, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Filter out any unsafe characters from a potential thumbprint
+        /// </summary>
+        /// <remarks>When copying a thumbprint from MMC, it ends up with a zero with uncode 
+        /// character (actually, a left-to-right indicator) which can get in the way of finding a 
+        /// certificate. Filtering the string helps to avoid this problem. 
+        /// See http://stackoverflow.com/a/14852713/30280 for more.</remarks>
+        /// <param name="thumbprint">Thumbprint to sanitize.</param>
+        /// <returns>Sanitized string.</returns>
+        private string SanitizeThumbprint(string thumbprint)
+        {
+            var builder = new StringBuilder();
+            foreach (var c in thumbprint.Where(char.IsLetterOrDigit))
+            {
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/CertificateThumbprintTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public class CertificateThumbprintTests
+    {
+        // One thumbprint string to use for testing
+        protected readonly string _thumbprintA = "NO TA CE RT IF IC AT EN OT HI NG TO SE EH ER EM OV EA LO NG";
+
+        // Another thumbprint string to use for testing
+        protected readonly string _thumbprintB = "TH IS IS NO TT HE CE RT IF IC AT EY OU AR EL OO KI NG FO RX";
+
+        // A thumbprint for testing
+        protected readonly CertificateThumbprint _testThumbprint;
+
+        // Another thumbprint for testing, identical to the one above
+        protected readonly CertificateThumbprint _identicalThumbprint;
+
+        // Another thumbprint, this one differs from the two above
+        protected readonly CertificateThumbprint _otherThumbprint;
+
+        public CertificateThumbprintTests()
+        {
+            _testThumbprint = new CertificateThumbprint(_thumbprintA);
+            _identicalThumbprint = new CertificateThumbprint(_thumbprintA);
+            _otherThumbprint = new CertificateThumbprint(_thumbprintB);
+        }
+
+        public class Constructor : CertificateThumbprintTests
+        {
+            [Fact]
+            public void GivenNull_ThrowsException()
+            {
+                Assert.Throws<ArgumentNullException>(
+                    () => new CertificateThumbprint(null));
+            }
+        }
+
+        public class EqualsMethod : CertificateThumbprintTests
+        {
+            [Fact]
+            public void GivenNull_ReturnsFalse()
+            {
+                _testThumbprint.Equals(null).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenSelf_ReturnsTrue()
+            {
+                _testThumbprint.Equals(_testThumbprint).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenIdenticalThumbprint_ReturnsTrue()
+            {
+                _testThumbprint.Equals(_identicalThumbprint).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenDifferentThumbprint_ReturnsFalse()
+            {
+                _testThumbprint.Equals(_otherThumbprint).Should().BeFalse();
+            }
+        }
+
+        public class HasThumbprintMethod : CertificateThumbprintTests
+        {
+            [Fact]
+            public void GivenNull_ReturnsFalse()
+            {
+                _testThumbprint.HasThumbprint(null).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenEmptyString_ReturnsFalse()
+            {
+                _testThumbprint.HasThumbprint(string.Empty).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenSameThumbprint_ReturnsTrue()
+            {
+                _testThumbprint.HasThumbprint(_thumbprintA).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenDifferentThumbprint_ReturnsFalse()
+            {
+                _testThumbprint.HasThumbprint(_thumbprintB).Should().BeFalse();
+            }
+        }
+    }
+}
