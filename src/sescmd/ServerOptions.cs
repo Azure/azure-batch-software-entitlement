4b7f6b4f8a535a9753fbd445ab22ba88c511c1bb
Bevan Arps <bevan.arps@microsoft.com>
2017-03-13 14:29:55 +1300

    Rework options available for server mode

--- src/sescmd/ServerOptions.cs
+++ src/sescmd/ServerOptions.cs
@@ -8,14 +8,22 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement
     [Verb("server", HelpText = "Run as a standalone software entitlement server.")]
     public class ServerOptions : OptionsBase
     {
-        [Option('s', "signature", HelpText = "Certificate thumbprint of the certificate used to sign the token.")]
-        public string SignatureThumbprint { get; set; }
+        [Option("signing-cert", HelpText = "Certificate thumbprint of the certificate used to sign the token (optional).")]
+        public string SigningCertificateThumbprint { get; set; }
 
         //TODO: Document where this looks to find the certificate (in a cross platform way)
-        [Option('e', "encrypt", HelpText = "Certificate thumbprint of the certificate used to encrypt the token.")]
-        public string EncryptionThumbprint { get; set; }
+        [Option("encryption-cert", HelpText = "Certificate thumbprint of the certificate used to encrypt the token (optional).")]
+        public string EncryptionCertificateThumbprint { get; set; }
 
-        [Option('x', "exit-after-request", HelpText = "Request the server exits after serving one request.")]
+        [Option("exit-after-request", HelpText = "Request the server exits after serving one request (optional).")]
         public bool ExitAfterRequest { get; set; }
+
+        [Option("connection-cert", HelpText = "Thumbprint of the certificate to pin for use with HTTPS (mandatory).")]
+        public string ConnectionCertificateThumbprint { get; set; }
+
+        [Option("url", HelpText = "The url at which the server should process requests (defaults to https://localhost:4443; must start with 'https:').")]
+        public string ServerUrl { get; set; } = "https://localhost:4443";
+
+
     }
 }
