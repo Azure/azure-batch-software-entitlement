8cdc1dd90178f5669d8d349837ec392057cce0a1
Bevan Arps <bevan.arps@microsoft.com>
2017-03-13 14:26:53 +1300

    Create extensions for Uri

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/UriExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
+{
+    /// <summary>
+    /// Extension methods for working with Uris
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Check to see if the Uri has a specific scheme
+        /// </summary>
+        /// <param name="uri">Uri to check.</param>
+        /// <param name="scheme">Scheme of interest.</param>
+        /// <returns>True if the uri has the specified scheme; false otherwise.</returns>
+        public static bool HasScheme(this Uri uri, string scheme)
+        {
+            return string.Equals(uri.Scheme, scheme, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
