77045a969110c6a9d8c0df2615b92fc01956499e
Bevan Arps <bevan.arps@microsoft.com>
2017-03-14 14:54:39 +1300

    Create Errorable and test

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/Errorable.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
+{
+    /// <summary>
+    /// A container that either contains a value or a set or errors
+    /// </summary>
+    public abstract class Errorable<T>
+    {
+        /// <summary>
+        /// Create a value that represents a successful operation with a result
+        /// </summary>
+        /// <param name="value">Result value to wrap.</param>
+        public static Errorable<T> Success(T value)
+        {
+            return new SuccessImplementation(value);
+        }
+
+        /// <summary>
+        /// Create a value that represents a failed operation
+        /// </summary>
+        /// <param name="errors">Sequence of error messages.</param>
+        public static Errorable<T> Failure(IEnumerable<string> errors)
+        {
+            var errorSet = errors.Aggregate(
+                ImmutableHashSet<string>.Empty,
+                (s, e) => s.Add(e));
+            return new FailureImplementation(errorSet);
+        }
+
+        /// <summary>
+        /// Create a value that represents a failed operation
+        /// </summary>
+        /// <param name="error">Sequence of error messages.</param>
+        public static Errorable<T> Failure(string error)
+        {
+            var errors = ImmutableHashSet<string>.Empty.Add(error);
+            return new FailureImplementation(errors);
+        }
+
+        /// <summary>
+        /// Gets a value indicating if we have a value
+        /// </summary>
+        public abstract bool HasValue { get; }
+
+        /// <summary>
+        /// Gets the value wrapped by this <see cref="Errorable{T}"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If no value is available.</exception>
+        public abstract T Value { get; }
+
+        /// <summary>
+        /// Gets the (possibly empty) set of errors reported
+        /// </summary>
+        public abstract ImmutableHashSet<string> Errors { get; }
+
+        /// <summary>
+        /// Add an error
+        /// </summary>
+        /// <remarks>Will abandon any wrapped value if this is the first error encountered.</remarks>
+        /// <param name="error">Error to record.</param>
+        /// <returns>A new instance with the error included.</returns>
+        public abstract Errorable<T> AddError(string error);
+
+        /// <summary>
+        /// Apply a transformation to our wrapped value, using another <see cref="Errorable{T}"/> 
+        /// as a parameter
+        /// </summary>
+        /// <param name="parameter">Parameter value to use in the transformation.</param>
+        /// <param name="transform">The transformation to apply.</param>
+        /// <returns></returns>
+        public abstract Errorable<R> Apply<X, R>(Errorable<X> parameter, Func<T, X, R> transform);
+
+        /// <summary>
+        /// An implementation of <see cref="Errorable{T}"/> that represents an actual value
+        /// </summary>
+        private class SuccessImplementation : Errorable<T>
+        {
+            public SuccessImplementation(T value)
+            {
+                Value = value;
+            }
+
+            public override bool HasValue => true;
+
+            public override T Value { get; }
+
+            public override ImmutableHashSet<string> Errors => ImmutableHashSet<string>.Empty;
+
+            public override Errorable<T> AddError(string error)
+            {
+                var errors = ImmutableHashSet<string>.Empty.Add(error);
+                return new FailureImplementation(errors);
+            }
+
+            public override Errorable<R> Apply<X, R>(Errorable<X> parameter, Func<T, X, R> transform)
+            {
+                return parameter.HasValue
+                    ? Errorable<R>.Success(transform(Value, parameter.Value))
+                    : new Errorable<R>.FailureImplementation(Errors.Union(parameter.Errors));
+            }
+        }
+
+        /// <summary>
+        /// An implementation of <see cref="Errorable{T}"/> that represents a set of errors from a 
+        /// failed operation
+        /// </summary>
+        private sealed class FailureImplementation : Errorable<T>
+        {
+            public FailureImplementation(ImmutableHashSet<string> errors)
+            {
+                Errors = errors;
+            }
+
+            public override bool HasValue => false;
+
+            public override T Value => throw new InvalidOperationException($"No value of type {typeof(T).Name} available.");
+
+            public override ImmutableHashSet<string> Errors { get; }
+
+            public override Errorable<T> AddError(string error)
+            {
+                return new FailureImplementation(Errors.Add(error));
+            }
+
+            public override Errorable<R> Apply<X, R>(Errorable<X> parameter, Func<T, X, R> transform)
+            {
+                return new Errorable<R>.FailureImplementation(Errors.Union(parameter.Errors));
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/ErrorableTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public class ErrorableTests
+    {
+        public class SuccessMethod : ErrorableTests
+        {
+            [Fact]
+            public void GivenValue_ReturnsResultWithValue()
+            {
+                var result = Errorable<int>.Success(42);
+                result.HasValue.Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenValue_ReturnsResultWithNoErrors()
+            {
+                var result = Errorable<int>.Success(42);
+                result.Errors.Should().HaveCount(0);
+            }
+        }
+
+        public class FailureMethod : ErrorableTests
+        {
+            [Fact]
+            public void GivenSingleValue_ReturnsResultWithoutValue()
+            {
+                var result = Errorable<int>.Failure("Error");
+                result.HasValue.Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenSingleValue_ReturnsResultWithOneError()
+            {
+                var result = Errorable<int>.Failure("Error");
+                result.Errors.Should().HaveCount(1);
+            }
+
+            [Fact]
+            public void GivenListOfSingleValue_ReturnsResultWithoutValue()
+            {
+                var result = Errorable<int>.Failure(new List<string> {"Error"});
+                result.HasValue.Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenListOfSingleValue_ReturnsResultWithOneError()
+            {
+                var result = Errorable<int>.Failure(new List<string> { "Error" });
+                result.Errors.Should().HaveCount(1);
+            }
+        }
+
+        public class AddErrorMethod : ErrorableTests
+        {
+            // Reference to a success to use for testing
+            private readonly Errorable<int> _success = Errorable<int>.Success(42);
+
+            // Reference to a failure to use for testing
+            private readonly Errorable<int> _failure = Errorable<int>.Failure("Error");
+
+            [Fact]
+            public void WhenSuccess_DiscardsValue()
+            {
+                var result = _success.AddError("Not the answer");
+                result.HasValue.Should().BeFalse();
+            }
+
+            [Fact]
+            public void WhenSuccess_IncludesError()
+            {
+                var errorMessage = "Not the answer";
+                var result = _success.AddError(errorMessage);
+                result.Errors.Should().Contain(errorMessage);
+            }
+
+            [Fact]
+            public void WhenFailure_IncludesError()
+            { 
+                var errorMessage = "Not the answer";
+                var result = _failure.AddError(errorMessage);
+                result.Errors.Should().Contain(errorMessage);
+            }
+        }
+    }
+}
