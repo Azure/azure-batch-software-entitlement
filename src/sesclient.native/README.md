1cf085f62000621410601b48abb911802d848076
Fab Tillier <ftillier@microsoft.com>
2017-03-15 16:38:24 +1300

    SES Native Client

---
+++ src/sesclient.native/README.md
@@ -0,0 +1,18 @@
+# Software entitlement service test client (sesclient.native)
+
+This command line executable demonstrates use of the [native-code software entitlement library](../Microsoft.Azure.Batch.SoftwareEntitlement.Client.Native).
+
+**This is draft documentation subject to change.**
+
+## Working with sesclient.native
+This executable is intended to be used in conjunction with the [sestest command line utility](../sestest) to generate tokens.
+
+The executable expects the following parameters (in any order):
+
+| Parameter            | Required  | Definition                                                                                                                         |
+| -------------------- | --------- | ---------------------------------------------------------------------------------------------------------------------------------- |
+| --url                | Mandatory | The URL at which the server should process requests. <p/> **Note**: must start with `https:`.                                      |
+| --thumbprint         | Mandatory | Thumbprint of a certificatein the server's SSL certificate chain. <p/> **Note**: cannot be the thumbprint of the root certificate. |
+| --common-name        | Mandatory | The common name of the certificate indicated by `--thumbprint`.                                                                    |
+| --token              | Mandatory | The token as returned by `sestest generate`.                                                                                       |
+| --application        | Mandatory | Unique identifier for the application being requested.                                                                             |
---
+++ src/sesclient.native/sesclient.native.cpp
@@ -0,0 +1,94 @@
+// sesclient.native.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+void ShowUsage(const char* exeName)
+{
+    std::cout << exeName << ":" << std::endl;
+    std::cout << "Contacts the specified azure batch software entitlement server to verify the provided token." << std::endl;
+    std::cout << "Parameters:" << std::endl;
+    std::cout << "    --url <software entitlement server URL>" << std::endl;
+    std::cout << "    --thumbprint <thumbprint of a certificate expected in the server's SSL certificate chain>" << std::endl;
+    std::cout << "    --common-name <common name of the certificate with the specified thumbprint>" << std::endl;
+    std::cout << "    --token <software entitlement token to pass to the server>" << std::endl;
+    std::cout << "    --application <name of the license ID being requested>" << std::endl;
+}
+
+std::vector<std::string> parameterNames = {
+    "--url",
+    "--thumbprint",
+    "--common-name",
+    "--token",
+    "--application"
+};
+
+
+struct Initializer
+{
+    Initializer()
+    {
+        int err = Microsoft::Azure::Batch::SoftwareEntitlement::Init();
+        if (err != 0)
+        {
+            throw std::runtime_error(
+                "Microsoft::Azure::Batch::SoftwareEntitlement::Init failed with error " +
+                std::to_string(err)
+            );
+        }
+    }
+
+    ~Initializer()
+    {
+        Microsoft::Azure::Batch::SoftwareEntitlement::Cleanup();
+    }
+};
+
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        Initializer init;
+
+        if (argc != 11)
+        {
+            ShowUsage(argv[0]);
+            return -1;
+        }
+
+        std::unordered_map<std::string, std::string> parameters;
+        while (argc != 1)
+        {
+            auto value = argv[--argc];
+            auto key = argv[--argc];
+            parameters.emplace(key, value);
+        }
+
+        for (const auto& param : parameterNames)
+        {
+            if (parameters.find(param) == parameters.end())
+            {
+                std::cout << "Missing parameter " << param << std::endl;
+                return -EINVAL;
+            }
+        }
+
+        auto entitlement = Microsoft::Azure::Batch::SoftwareEntitlement::GetEntitlement(
+            parameters.find("--url")->second,
+            parameters.find("--thumbprint")->second,
+            parameters.find("--common-name")->second,
+            parameters.find("--token")->second,
+            parameters.find("--application")->second
+        );
+        std::cout << entitlement->Id() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}
+
