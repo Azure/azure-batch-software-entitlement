db129594accaa48a2b3b2abafe0dcba19c87efcd
Bevan Arps <bevan.arps@microsoft.com>
2017-03-26 19:34:36 +1300

    Rename variables for clarity in cover.ps1

--- cover.ps1
+++ cover.ps1
@@ -9,14 +9,14 @@ function write-header($header) {
 }
 
 write-line
-$dotnet = resolve-path "C:\Program Files\dotnet\dotnet.exe"
-Write-Host "Dot net runtime:             $dotnet"
+$dotnetExe = resolve-path "C:\Program Files\dotnet\dotnet.exe"
+Write-Host "Dotnet executable:           $dotnetExe"
 
-$openCover = resolve-path $env:userprofile\.nuget\packages\OpenCover\*\tools\OpenCover.Console.exe 
-Write-Host "Opencover executable:        $openCover"
+$openCoverExe = resolve-path $env:userprofile\.nuget\packages\OpenCover\*\tools\OpenCover.Console.exe 
+Write-Host "Opencover executable:        $openCoverExe"
 
-$reportGenerator = resolve-path $env:userprofile\.nuget\packages\reportgenerator\*\tools\ReportGenerator.exe
-Write-Host "Report Generator executable: $reportGenerator"
+$reportGeneratorExe = resolve-path $env:userprofile\.nuget\packages\reportgenerator\*\tools\ReportGenerator.exe
+Write-Host "Report Generator executable: $reportGeneratorExe"
 write-line
 
 $commonTests = resolve-path .\tests\Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests\*.csproj
@@ -26,13 +26,13 @@ $logLevel = "info"
 $filter = "+[*]* -[xunit.*]* -[Fluent*]* -[*.Tests]* -[sestest]*"
 
 Write-Header "Running tests for: $commonTests"
-& $openCover -oldStyle -target:$dotnet -targetargs:"test $commonTests" -register:user -filter:$filter -log:$loglevel -output:.\out\Common.cover.xml
+& $openCoverExe -oldStyle -target:$dotnetExe -targetargs:"test $commonTests" -register:user -filter:$filter -log:$loglevel -output:.\out\Common.cover.xml
 
 Write-Header "Running tests for $sesTests"
-& $openCover -oldStyle -target:$dotnet -targetargs:"test $sesTests" -register:user -filter:$filter -log:$loglevel -output:.\out\Ses.cover.xml
+& $openCoverExe -oldStyle -target:$dotnetExe -targetargs:"test $sesTests" -register:user -filter:$filter -log:$loglevel -output:.\out\Ses.cover.xml
 
 Write-Header "Generating Report"
-& $reportGenerator "-reports:.\out\Common.cover.xml;.\out\Ses.cover.xml" "-targetdir:.\out\cover\"
+& $reportGeneratorExe "-reports:.\out\Common.cover.xml;.\out\Ses.cover.xml" "-targetdir:.\out\cover\"
 
 $reportIndex = resolve-path .\out\cover\index.htm
 write-line
