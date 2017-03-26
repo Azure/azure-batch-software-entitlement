432c882e3b591b9150a9f775fdd813645e551c4c
Bevan Arps <bevan.arps@outlook.com>
2017-03-26 19:36:32 +1300

    Improve unit test coverage (#18)
    * Extract AsLines() and add tests
    * Add test coverage for UnpackingExceptionLogger
    * Use OpenCover to review test coverage
    * Add test coverage for SoftwareEntitlement
    * Add scripts to build and clean

---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/StringExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public class StringExtensionsTests
+    {
+        public class AsLinesMethod : StringExtensionsTests
+        {
+            [Theory]
+            [InlineData("A simple string", 1)]
+            [InlineData("A simple string\nWith another line", 2)]
+            [InlineData("A simple string\r\nWith another line", 2)]
+            [InlineData("Alpha\nBeta\nGamma\nDelta\nEpsilon\nSigma\nPhi", 7)]
+            [InlineData("A simple string with EOLN at the end is one line\n", 1)]
+            public void GivenString_ReturnsExpectedCount(string content, int count)
+            {
+                content.AsLines().Should().HaveCount(count);
+            }
+
+            [Theory]
+            [InlineData("A simple string")]
+            [InlineData("A simple string\nWith another line\n")]
+            [InlineData("A simple string\r\nWith another line\r\n")]
+            [InlineData("Alpha\nBeta\nGamma\nDelta\nEpsilon\nSigma\nPhi")]
+            [InlineData("A simple string with EOLN at the end is one line\n")]
+            public void GivenString_NoLinesEndWithEolnCharacters(string content)
+            {
+                var lines = content.AsLines().ToList();
+                lines.Should().NotContain(l => l.EndsWith("\r"));
+                lines.Should().NotContain(l => l.EndsWith("\n"));
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Tests/SoftwareEntitlementTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Tests
+{
+    public class SoftwareEntitlementTests
+    {
+        // An empty software entitlement to use for testing
+        private readonly SoftwareEntitlement _emptyEntitlement = new SoftwareEntitlement(NullLogger.Instance);
+
+        // A Times span representing NZDT
+        private readonly TimeSpan _nzdt = new TimeSpan(+13, 0, 0);
+
+        // An instant to use as the start for testing
+        private DateTimeOffset _start;
+
+        // An instant to use as the finish for testing
+        private DateTimeOffset _finish;
+
+        public SoftwareEntitlementTests()
+        {
+            _start = new DateTimeOffset(2016, 2, 29, 16, 14, 12, _nzdt);
+            _finish = new DateTimeOffset(2016, 3, 31, 16, 14, 12, _nzdt);
+        }
+
+        public class WithVirtualMachineIdMethod : SoftwareEntitlementTests
+        {
+            [Fact]
+            public void GivenNull_ThrowsArgumentNullException()
+            {
+                Assert.Throws<ArgumentNullException>(
+                    () => _emptyEntitlement.WithVirtualMachineId(null));
+            }
+
+            [Fact]
+            public void GivenVirtualMachineId_ConfiguresProperty()
+            {
+                var vmid = "Sample";
+                _emptyEntitlement.WithVirtualMachineId(vmid)
+                    .VirtualMachineId.Should()
+                    .Be(vmid);
+            }
+        }
+
+        public class ForTimeRangeMethod : SoftwareEntitlementTests
+        {
+            [Fact]
+            public void GivenStart_ConfiguresProperty()
+            {
+                _emptyEntitlement.ForTimeRange(_start, _finish)
+                    .NotBefore
+                    .Should()
+                    .Be(_start);
+            }
+
+            [Fact]
+            public void GivenFinish_ConfiguresProperty()
+            {
+                _emptyEntitlement.ForTimeRange(_start, _finish)
+                    .NotAfter
+                    .Should()
+                    .Be(_finish);
+            }
+        }
+    }
+}
