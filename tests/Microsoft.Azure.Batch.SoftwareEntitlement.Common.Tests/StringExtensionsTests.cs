51c00a6aa41bb3bb40005b4cd73b59188deb5755
Bevan Arps <bevan.arps@microsoft.com>
2017-03-26 19:35:04 +1300

    Add new test for StringExtensions.AsLines()

--- tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/StringExtensionsTests.cs
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/StringExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using FluentAssertions;
 using Xunit;
@@ -19,6 +20,19 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
             public void GivenString_ReturnsExpectedCount(string content, int count)
             {
                 content.AsLines().Should().HaveCount(count);
+            }
+
+            [Theory]
+            [InlineData("A simple string")]
+            [InlineData("A simple string\nWith another line\n")]
+            [InlineData("A simple string\r\nWith another line\r\n")]
+            [InlineData("Alpha\nBeta\nGamma\nDelta\nEpsilon\nSigma\nPhi")]
+            [InlineData("A simple string with EOLN at the end is one line\n")]
+            public void GivenString_NoLinesEndWithEolnCharacters(string content)
+            {
+                var lines = content.AsLines().ToList();
+                lines.Should().NotContain(l => l.EndsWith("\r"));
+                lines.Should().NotContain(l => l.EndsWith("\n"));
             }
         }
     }
