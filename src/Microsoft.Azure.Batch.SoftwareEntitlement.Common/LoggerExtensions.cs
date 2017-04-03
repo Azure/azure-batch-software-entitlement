cbdbf028395df0cb8477dbf2f1660c491f9da576
Bevan Arps <bevan.arps@microsoft.com>
2017-04-03 12:20:09 +1200

    Add LogTable() extension

---
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/LoggerExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
+{
+    /// <summary>
+    /// Extensions for working with logging
+    /// </summary>
+    public static class LoggerExtensions
+    {
+        /// <summary>
+        /// Log a series of lines as a table, splitting at tabs
+        /// </summary>
+        /// <param name="logger">Actual logger to use.</param>
+        /// <param name="level">Log level for the output.</param>
+        /// <param name="lines">Lines to format.</param>
+        public static void LogTable(this ILogger logger, LogLevel level, IEnumerable<string> lines)
+        {
+            var rows = lines.Select(l => l.Split('\t')).ToList();
+            var columns = rows.Max(r => r.Length);
+            var widths = Enumerable.Range(0, columns)
+                .Select(index => rows.Max(r => index < r.Length ? r[index].Length : 0) + 3)
+                .ToList();
+
+            string FormatRow(string[] cells, Exception exception)
+            {
+                return string.Join("", cells.Select((cell, index) => cell.PadRight(widths[index])))
+                    .TrimEnd();
+            }
+
+            foreach (var row in rows)
+            {
+                logger.Log(level, (EventId)0, row, null, FormatRow);
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/LoggerExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests.Fakes;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
+{
+    public abstract class LoggerExtensionsTests
+    {
+        public class LogTable : LoggerExtensionsTests
+        {
+            private readonly CollectingLogger _logger = new CollectingLogger();
+
+            [Fact]
+            public void GivenOneColumn_LogsExpectedRows()
+            {
+                var lines = new List<string>
+                {
+                    "alpha",
+                    "beta",
+                    "gamma",
+                    "delta"
+                };
+                _logger.LogTable(LogLevel.Information, lines);
+                _logger.Events.Should().HaveCount(4);
+                _logger.Events.Should().OnlyContain(e => e.Level == LogLevel.Information);
+                _logger.Events.Select(e => e.Message).Should().ContainInOrder(lines);
+            }
+
+            [Fact]
+            public void GivenTwoColumns_LogsExpectedRows()
+            {
+                var lines = new List<string>
+                {
+                    "alpha\tMercury",
+                    "beta\tVenus",
+                    "gamma\tEarth",
+                    "delta\tMars",
+                    "epsilon\tJupiter",
+                    "sigma\tSaturn",
+                    "phi\tUranus",
+                    "omicron\tNeptune"
+                };
+                _logger.LogTable(LogLevel.Information, lines);
+                _logger.Events.Should().HaveCount(8);
+                _logger.Events.Should().OnlyContain(e => e.Level == LogLevel.Information);
+                _logger.Events.Select(e => e.Message).Should().ContainInOrder(
+                    "alpha     Mercury",
+                    "beta      Venus",
+                    "gamma     Earth",
+                    "delta     Mars",
+                    "epsilon   Jupiter",
+                    "sigma     Saturn",
+                    "phi       Uranus",
+                    "omicron   Neptune");
+            }
+
+            [Fact]
+            public void GivenStringsWithBlankLeadingColumns_LogsExpectedRows()
+            {
+                var lines = new List<string>
+                {
+                    "alpha\tMercury",
+                    "beta\tVenus",
+                    "\tEarth",
+                    "delta\tMars",
+                    "\tJupiter",
+                    "sigma\tSaturn",
+                    "phi\tUranus",
+                    "omicron\tNeptune"
+                };
+                _logger.LogTable(LogLevel.Information, lines);
+                _logger.Events.Should().HaveCount(8);
+                _logger.Events.Should().OnlyContain(e => e.Level == LogLevel.Information);
+                _logger.Events.Select(e => e.Message).Should().ContainInOrder(
+                    "alpha     Mercury",
+                    "beta      Venus",
+                    "          Earth",
+                    "delta     Mars",
+                    "          Jupiter",
+                    "sigma     Saturn",
+                    "phi       Uranus",
+                    "omicron   Neptune");
+            }
+        }
+    }
+}
