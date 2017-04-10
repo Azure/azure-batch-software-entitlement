123899efc5fbc92636cd7af20029f520a1e4a0a9
Bevan Arps <bevan.arps@microsoft.com>
2017-04-11 11:49:22 +1200

    Define Combine(Errorable, Errorable, Func) and test

--- src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/ErrorableExtensions.cs
+++ src/Microsoft.Azure.Batch.SoftwareEntitlement.Common/ErrorableExtensions.cs
@@ -91,6 +91,51 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common
             var allErrors = first.Errors.Union(second.Errors);
             return Errorable.Failure<R>(allErrors);
         }
+        
+        /// <summary>
+        /// Combine three <see cref="Errorable{T}"/> values and return a result.
+        /// </summary>
+        /// <remarks>Any errors already present are preserved.</remarks>
+        /// <typeparam name="T">Type of value possibly present in <paramref name="first"/>.</typeparam>
+        /// <typeparam name="A">Type of value possibly present in <paramref name="second"/>.</typeparam>
+        /// <typeparam name="B">Type of value possibly present in <paramref name="second"/>.</typeparam>
+        /// <param name="first">First errorable value.</param>
+        /// <param name="second">Second errorable value.</param>
+        /// <param name="third">Third errorable value.</param>
+        /// <param name="whenSuccessful">Function to combine all three values when available.</param>
+        public static Errorable<R> Combine<T, A, B, R>(
+            this Errorable<T> first,
+            Errorable<A> second,
+            Errorable<B> third,
+            Func<T, A, B, R> whenSuccessful)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+
+            if (third == null)
+            {
+                throw new ArgumentNullException(nameof(third));
+            }
+
+            if (whenSuccessful == null)
+            {
+                throw new ArgumentNullException(nameof(whenSuccessful));
+            }
+
+            if (first.HasValue && second.HasValue && third.HasValue)
+            {
+                return Errorable.Success(whenSuccessful(first.Value, second.Value, third.Value));
+            }
+
+            var allErrors = first.Errors.Union(second.Errors).Union(third.Errors);
+            return Errorable.Failure<R>(allErrors);
         }
     }
 }
--- tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/ErrorableExtensionTests.cs
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/ErrorableExtensionTests.cs
@@ -254,6 +254,240 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
         public class CombineWithTwoErrorablesAndFuncs : ErrorableExtensionTests
         {
             // Known errorable values for testing
+            private readonly Errorable<int> _missing = null;
+            private readonly Errorable<int> _success = Errorable.Success(4);
+            private readonly Errorable<int> _failure = Errorable.Failure<int>("failure");
+            private readonly Errorable<int> _otherSuccess = Errorable.Success(2);
+            private readonly Errorable<int> _otherFailure = Errorable.Failure<int>("other failure");
+            private readonly Errorable<int> _yetAnotherSuccess = Errorable.Success(8);
+            private readonly Errorable<int> _yetAnotherFailure = Errorable.Failure<int>("yet another failure");
+
+            [Fact]
+            public void GivenNullForFirstErrorable_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _missing.Combine(_otherSuccess, _yetAnotherSuccess, WhenSuccessfulDoNothing));
+                exception.ParamName.Should().Be("first");
+            }
+
+            [Fact]
+            public void GivenNullForSecondErrorable_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _success.Combine(_missing, _yetAnotherSuccess, WhenSuccessfulDoNothing));
+                exception.ParamName.Should().Be("second");
+            }
+
+            [Fact]
+            public void GivenNullForThirdErrorable_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _success.Combine(_otherSuccess, _missing, WhenSuccessfulDoNothing));
+                exception.ParamName.Should().Be("third");
+            }
+
+            [Fact]
+            public void GivenNullForSuccessAction_ThrowsArgumentNullException()
+            {
+                Func<int, int, int, string> whenSuccessful = null;
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _success.Combine(_otherSuccess, _yetAnotherSuccess, whenSuccessful));
+                exception.ParamName.Should().Be("whenSuccessful");
+            }
+
+            [Fact]
+            public void GivenSuccessAndSuccessAndSuccess_CallsSuccessActionWithExpectedValues()
+            {
+                var receivedFu = 0;
+                var receivedBar = 0;
+                var receivedBaz = 0;
+
+                int WhenSuccessful(int fu, int bar, int baz)
+                {
+                    receivedFu = fu;
+                    receivedBar = bar;
+                    receivedBaz = baz;
+                    return (fu * 100) + (bar * 10) + baz;
+                }
+
+                _success.Combine(_otherSuccess, _yetAnotherSuccess, WhenSuccessful);
+
+                receivedFu.Should().Be(_success.Value);
+                receivedBar.Should().Be(_otherSuccess.Value);
+                receivedBaz.Should().Be(_yetAnotherSuccess.Value);
+            }
+
+            [Fact]
+            public void GivenSuccessAndSuccessAndSuccess_ReturnsExpectedResult()
+            {
+                int WhenSuccessful(int fu, int bar, int baz)
+                {
+                    return fu * 100 + bar * 10 + baz;
+                }
+
+                var result = _success.Combine(_otherSuccess, _yetAnotherSuccess, WhenSuccessful);
+                result.HasValue.Should().BeTrue();
+                result.Value.Should().Be(428);
+            }
+
+            [Fact]
+            public void GivenSuccessAndSuccessAndFailure_ReturnsExistingErrors()
+            {
+                var result = _success.Combine(_otherSuccess, _yetAnotherFailure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_yetAnotherFailure.Errors);
+            }
+
+            [Fact]
+            public void GivenSuccessAndFailureAndSuccess_ReturnsExistingErrors()
+            {
+                var result = _success.Combine(_otherFailure, _yetAnotherSuccess, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_otherFailure.Errors);
+            }
+
+            [Fact]
+            public void GivenFailureAndSuccessAndSuccess_ReturnsExistingErrors()
+            {
+                var result = _failure.Combine(_otherSuccess, _yetAnotherSuccess, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_failure.Errors);
+            }
+
+            [Fact]
+            public void GivenFailureAndFailureAndFailure_ReturnsCombinedErrors()
+            {
+                var result = _failure.Combine(_otherFailure, _yetAnotherFailure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(
+                    _failure.Errors.Union(_otherFailure.Errors).Union(_yetAnotherFailure.Errors));
+            }
+
+            [Fact]
+            public void GivenFailureAndFailureAndFailure_CallsFailureActionWithUniqueErrors()
+            {
+                var result = _failure.Combine(_failure, _failure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_failure.Errors);
+            }
+
+            // A safe default action for when a test doesn't care about success
+            private void WhenSuccessfulDoNothing(int left, int right) { }
+
+            // A safe default action for when a test doesn't care about failure
+            private void WhenFailureDoNothing(IEnumerable<string> errors) { }
+
+            // A success action that throws an exception to fail a test
+            private void WhenSuccessfullAbort(int left, int right)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // A failure action that throws an exception to fail a test
+            private void WhenFailureAbort(IEnumerable<string> errors)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public class CombineWithErrorableAndFuncs : ErrorableExtensionTests
+        {
+            // Known errorable values for testing
+            private readonly Errorable<int> _nullErrorable = null;
+            private readonly Errorable<int> _firstSuccess = Errorable.Success(4);
+            private readonly Errorable<int> _firstFailure = Errorable.Failure<int>("Failure the first");
+            private readonly Errorable<int> _secondSuccess = Errorable.Success(2);
+            private readonly Errorable<int> _secondFailure = Errorable.Failure<int>("Failure the second");
+
+            [Fact]
+            public void GivenNullForFirstErrorable_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _nullErrorable.Combine(_secondSuccess, WhenSuccessfulDoNothing));
+                exception.ParamName.Should().Be("first");
+            }
+
+            [Fact]
+            public void GivenNullForSecondErrable_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _firstSuccess.Combine(_nullErrorable, WhenSuccessfulDoNothing));
+                exception.ParamName.Should().Be("second");
+            }
+
+            [Fact]
+            public void GivenNullForSuccessAction_ThrowsArgumentNullException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _firstSuccess.Combine(_secondSuccess, (Func<int, int, int>) null));
+                exception.ParamName.Should().Be("whenSuccessful");
+            }
+
+            [Fact]
+            public void GivenSuccessAndSuccess_CallsSuccessActionWithExpectedValues()
+            {
+                var receivedLeft = 0;
+                var receivedRight = 0;
+
+                int WhenSuccessful(int left, int right)
+                {
+                    receivedLeft = left;
+                    receivedRight = right;
+                    return left * 10 + right;
+                }
+
+                _firstSuccess.Combine(_secondSuccess, WhenSuccessful);
+
+                receivedLeft.Should().Be(_firstSuccess.Value);
+                receivedRight.Should().Be(_secondSuccess.Value);
+            }
+
+            [Fact]
+            public void GivenSuccessAndSuccess_ReturnsExpectedResult()
+            {
+                var result = _firstSuccess.Combine(_secondSuccess, (l, r) => l * 10 + r);
+                result.HasValue.Should().BeTrue();
+                result.Value.Should().Be(42);
+            }
+
+            [Fact]
+            public void GivenSuccessAndFailure_ReturnsExistingErrors()
+            {
+                var result = _firstSuccess.Combine(_secondFailure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_secondFailure.Errors);
+            }
+
+            [Fact]
+            public void GivenFailureAndSuccess_ReturnsExistingErrors()
+            {
+                var result = _firstFailure.Combine(_secondSuccess, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors);
+            }
+
+            [Fact]
+            public void GivenFailureAndFailure_ReturnsCombinedErrors()
+            {
+                var result = _firstFailure.Combine(_secondFailure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors.Union(_secondFailure.Errors));
+            }
+
+            [Fact]
+            public void GivenFailureAndFailure_CallsFailureActionWithUniqueErrors()
+            {
+                var result = _firstFailure.Combine(_firstFailure, WhenSuccessfullAbort);
+                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors);
+            }
+
+            // A safe default action for when a test doesn't care about success
+
+            // A success action that throws an exception to fail a test
+        }
+
+        public class CombineWithTwoErrorablesAndFuncs : ErrorableExtensionTests
+        {
+            // Known errorable values for testing
             private readonly Errorable<int> _nullErrorable = null;
             private readonly Errorable<int> _firstSuccess = Errorable.Success(4);
             private readonly Errorable<int> _firstFailure = Errorable.Failure<int>("Failure the first");
@@ -370,118 +604,10 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
             }
 
             // A safe default action for when a test doesn't care about success
-            private void WhenSuccessfulDoNothing(int left, int right) { }
-
-            // A safe default action for when a test doesn't care about failure
-            private void WhenFailureDoNothing(IEnumerable<string> errors) { }
+            private int WhenSuccessfulDoNothing(int alpht, int beta, int gamma) => 0;
 
             // A success action that throws an exception to fail a test
-            private void WhenSuccessfullAbort(int left, int right)
-            {
-                throw new InvalidOperationException();
-            }
-
-            // A failure action that throws an exception to fail a test
-            private void WhenFailureAbort(IEnumerable<string> errors)
-            {
-                throw new InvalidOperationException();
-            }
-        }
-
-        public class CombineWithErrorableAndFuncs : ErrorableExtensionTests
-        {
-            // Known errorable values for testing
-            private readonly Errorable<int> _nullErrorable = null;
-            private readonly Errorable<int> _firstSuccess = Errorable.Success(4);
-            private readonly Errorable<int> _firstFailure = Errorable.Failure<int>("Failure the first");
-            private readonly Errorable<int> _secondSuccess = Errorable.Success(2);
-            private readonly Errorable<int> _secondFailure = Errorable.Failure<int>("Failure the second");
-
-            [Fact]
-            public void GivenNullForFirstErrorable_ThrowsArgumentNullException()
-            {
-                var exception =
-                    Assert.Throws<ArgumentNullException>(
-                        () => _nullErrorable.Combine(_secondSuccess, WhenSuccessfulDoNothing));
-                exception.ParamName.Should().Be("first");
-            }
-
-            [Fact]
-            public void GivenNullForSecondErrable_ThrowsArgumentNullException()
-            {
-                var exception =
-                    Assert.Throws<ArgumentNullException>(
-                        () => _firstSuccess.Combine(_nullErrorable, WhenSuccessfulDoNothing));
-                exception.ParamName.Should().Be("second");
-            }
-
-            [Fact]
-            public void GivenNullForSuccessAction_ThrowsArgumentNullException()
-            {
-                var exception =
-                    Assert.Throws<ArgumentNullException>(
-                        () => _firstSuccess.Combine(_secondSuccess, (Func<int, int, int>) null));
-                exception.ParamName.Should().Be("whenSuccessful");
-            }
-
-            [Fact]
-            public void GivenSuccessAndSuccess_CallsSuccessActionWithExpectedValues()
-            {
-                var receivedLeft = 0;
-                var receivedRight = 0;
-
-                int WhenSuccessful(int left, int right)
-                {
-                    receivedLeft = left;
-                    receivedRight = right;
-                    return left * 10 + right;
-                }
-
-                _firstSuccess.Combine(_secondSuccess, WhenSuccessful);
-
-                receivedLeft.Should().Be(_firstSuccess.Value);
-                receivedRight.Should().Be(_secondSuccess.Value);
-            }
-
-            [Fact]
-            public void GivenSuccessAndSuccess_ReturnsExpectedResult()
-            {
-                var result = _firstSuccess.Combine(_secondSuccess, (l, r) => l * 10 + r);
-                result.HasValue.Should().BeTrue();
-                result.Value.Should().Be(42);
-            }
-
-            [Fact]
-            public void GivenSuccessAndFailure_ReturnsExistingErrors()
-            {
-                var result = _firstSuccess.Combine(_secondFailure, WhenSuccessfullAbort);
-                result.Errors.Should().BeEquivalentTo(_secondFailure.Errors);
-            }
-
-            [Fact]
-            public void GivenFailureAndSuccess_ReturnsExistingErrors()
-            {
-                var result = _firstFailure.Combine(_secondSuccess, WhenSuccessfullAbort);
-                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors);
-            }
-
-            [Fact]
-            public void GivenFailureAndFailure_ReturnsCombinedErrors()
-            {
-                var result = _firstFailure.Combine(_secondFailure, WhenSuccessfullAbort);
-                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors.Union(_secondFailure.Errors));
-            }
-
-            [Fact]
-            public void GivenFailureAndFailure_CallsFailureActionWithUniqueErrors()
-            {
-                var result = _firstFailure.Combine(_firstFailure, WhenSuccessfullAbort);
-                result.Errors.Should().BeEquivalentTo(_firstFailure.Errors);
-            }
-
-            // A safe default action for when a test doesn't care about success
-
-            // A success action that throws an exception to fail a test
+            private int WhenSuccessfullAbort(int alkpha, int beta, int gamma) => throw new InvalidOperationException();
         }
     }
 }
