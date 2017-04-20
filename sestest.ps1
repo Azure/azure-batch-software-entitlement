1e50f4f5ff17c35bb4d47d72b5de136d3306f4ee
Bevan Arps <bevan.arps@microsoft.com>
2017-04-21 09:31:33 +1200

    Add script to run sesclient
    Also improve the script for sestest

--- sestest.ps1
+++ sestest.ps1
@@ -1,4 +1,10 @@
 #
 # Convenience Launcher for sestest
 #
-dotnet $PSScriptRoot\out\sestest\Debug\netcoreapp1.1\sestest.dll $args
+
+$sestest = "$PSScriptRoot\out\sestest\Debug\netcoreapp1.1\sestest.dll"
+
+# Uncomment this line to show which exe is being run
+# Write-Output "Launching $sestest"
+
+dotnet $sestest $args
