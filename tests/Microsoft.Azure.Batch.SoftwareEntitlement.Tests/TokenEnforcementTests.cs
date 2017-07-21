413d5ac10a3620ab10b4b9f66f0656627d836a1e
Bevan Arps <bevan.arps@outlook.com>
2017-07-21 14:34:43 +1200

    Run unit tests when building (#52)
    * Run tests as a part of the cross-platform build
    * Allow ServerOptionsBuilder to be less strict when testing

--- tests/Microsoft.Azure.Batch.SoftwareEntitlement.Tests/TokenEnforcementTests.cs
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Tests/TokenEnforcementTests.cs
@@ -386,7 +386,7 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Tests
 
         public class WithCertificates : TokenEnforcementTests
         {
-            [Theory(Skip = "Need a thumbprint specified in TestCaseKeys()")]
+            [Theory(Skip = "Specify a certificate thumbprint in TestCaseKeys() to enable this test.")]
             [MemberData(nameof(TestCaseKeys))]
             public void WhenSignedByCertificate_ReturnsExpectedResult(SecurityKey key)
             {
@@ -402,7 +402,7 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Tests
                 result.Value.Applications.Should().Contain(_contosoFinanceApp);
             }
 
-            [Theory(Skip = "Need a thumbprint specified in TestCaseKeys()")]
+            [Theory(Skip = "Specify a certificate thumbprint in TestCaseKeys() to enable this test.")]
             [MemberData(nameof(TestCaseKeys))]
             public void WhenEncryptedByCertificate_ReturnsExpectedResult(SecurityKey key)
             {
