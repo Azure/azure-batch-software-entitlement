31a48bbb055aa5cf55307c6f70d31c5f23a631de
Bevan Arps <bevan.arps@outlook.com>
2018-01-29 15:59:29 +1300

    Implement equality for Specifiable (#90)
    Implement equality for `Specifiable<T>`, including unit tests

--- tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/SpecifiableTests.cs
+++ tests/Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests/SpecifiableTests.cs
@@ -84,5 +84,101 @@ namespace Microsoft.Azure.Batch.SoftwareEntitlement.Common.Tests
                 result.Should().Be(_otherStringValue);
             }
         }
+
+        public class EqualsObject : SpecifiableTests
+        {
+            private readonly Specifiable<string> _sample = Specify.As("sample");
+
+            private readonly Specifiable<string> _other = Specify.As("other");
+
+            [Fact]
+            public void GivenSelf_ReturnsTrue()
+            {
+                _sample.Equals((object) _sample).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenOther_ReturnsFalse()
+            {
+                _sample.Equals((object)_other).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenNull_ReturnsFalse()
+            {
+                _sample.Equals(null).Should().BeFalse();
+            }
+        }
+
+        public class EqualsSpecifiable : SpecifiableTests
+        {
+            private readonly Specifiable<string> _sample = Specify.As("sample");
+
+            private readonly Specifiable<string> _other = Specify.As("other");
+
+            private readonly Specifiable<string> _null = Specify.As<string>(null);
+
+            private readonly Specifiable<string> _unspecified = new Specifiable<string>();
+
+            [Fact]
+            public void GivenSelf_ReturnsTrue()
+            {
+                _sample.Equals(_sample).Should().BeTrue();
+            }
+
+            [Fact]
+            public void GivenOther_ReturnsFalse()
+            {
+                _sample.Equals(_other).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenUnspecified_ReturnsFalse()
+            {
+                _sample.Equals(_unspecified).Should().BeFalse();
+            }
+
+            [Fact]
+            public void GivenNull_ReturnsFalse()
+            {
+                _sample.Equals(null).Should().BeFalse();
+            }
+
+            [Fact]
+            public void ContainingNullGivenSelf_ReturnsTrue()
+            {
+                _null.Equals(_null).Should().BeTrue();
+            }
+
+            [Fact]
+            public void ContainingNullGivenOther_ReturnsFalse()
+            {
+                _null.Equals(_other).Should().BeFalse();
+            }
+
+            [Fact]
+            public void ContainingNullGivenUnspecified_ReturnsFalse()
+            {
+                _null.Equals(_unspecified).Should().BeFalse();
+            }
+
+            [Fact]
+            public void WhenUnspecified_EqualsSelf()
+            {
+                _unspecified.Equals(_unspecified).Should().BeTrue();
+            }
+
+            [Fact]
+            public void WhenUnspecified_DoesNotEqualOther()
+            {
+                _unspecified.Equals(_other).Should().BeFalse();
+            }
+
+            [Fact]
+            public void WhenUnspecified_DoesNotEqualNull()
+            {
+                _unspecified.Equals(_null).Should().BeFalse();
+            }
+        }
     }
 }
