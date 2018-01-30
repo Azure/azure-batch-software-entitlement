96e0e4604f4cce5c464d333f74de800dc55a70c3
Bevan Arps <bevan.arps@outlook.com>
2018-01-30 13:05:59 +1300

    Modify convenience scripts to search for exe (#92)
    Removes hardcoded output paths and selects the most recently compiled exe automatically

--- sestest.ps1
+++ sestest.ps1
@@ -2,7 +2,18 @@
 # Convenience Launcher for sestest
 #
 
-$sestest = "$PSScriptRoot\out\sestest\netcoreapp2.0\sestest.dll"
+# Find the latest version of sestest that was compiled
+# [This will automatically find the right version, even if someone does a release 
+# build or if the output path changes e.g. becuase it's no longer netcoreapp2.0]
+$sestest = get-childitem $PSScriptRoot\out\sestest.dll -recurse | sort-object LastWriteTimeUtc | select-object -last 1
+
+if ($sestest -eq $null) 
+{
+    Write-Host "Could not find 'sestest.dll' within $PSScriptRoot\out" -ForegroundColor Red
+    Write-Host "Do you need to run a build?" -ForegroundColor Yellow
+    exit -1
+}
+
 
 # Uncomment this line to show which exe is being run
 # Write-Output "Launching $sestest"
