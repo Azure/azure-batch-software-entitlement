33c94f7a5480f49340514baf6418ef4aacb87904
Bevan Arps <bevan.arps@outlook.com>
2018-01-30 13:05:59 +1300

    Modify convenience scripts to search for exe (#92)
    Removes hardcoded output paths and selects the most recently compiled exe automatically

--- sesclient.ps1
+++ sesclient.ps1
@@ -2,7 +2,17 @@
 # Convenience Launcher for sesclient
 #
 
-$sesclient = "$PSScriptRoot\x64\Debug\sesclient.native.exe"
+# Find the latest version of sesclient.native.exe that was compiled
+# [This will automatically find the right version, even if someone does a release 
+# build or if the output path changes]
+$sesclient = get-childitem $PSScriptRoot\x64\sesclient.native.exe -recurse | sort-object LastWriteTimeUtc | select-object -last 1
+
+if ($sesclient -eq $null) 
+{
+    Write-Host "Could not find 'sesclient.native.exe' within $PSScriptRoot\x64" -ForegroundColor Red
+    Write-Host "Do you need to run a build?" -ForegroundColor Yellow
+    exit -1
+}
 
 # Uncomment this line to show which command is being run
 # Write-Output "Launching $sesclient $args"
