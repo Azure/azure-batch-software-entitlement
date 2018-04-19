a621e098a19649b6edad290447dcf67eb61d62b5
Bevan Arps <bearps@microsoft.com>
2018-04-19 12:52:21 +1200

    Update bootstrap.ps1 for finding psake

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -11,48 +11,48 @@ function TryLoad-Psake($path) {
         return
     }
 
+	$toNatural = { [regex]::Replace($_, '\d+', { $args[0].Value.PadLeft(20) }) }
+
     # Resolve the path we were given; in the case of multiple matches, take the "latest" one
-    $psakePath = resolve-path $path\psake.psm1 -ErrorAction SilentlyContinue | Sort-Object -Property "Name" | select-object -last 1
+    $psakePath = get-childitem $path\psake*\psake.psm1 -ErrorAction SilentlyContinue -Recurse | Sort-Object $toNatural | select-object -last 1
 
     if ($psakePath -eq $null) {
         Write-Output "[!] Psake not found at $path"
     } else {
         import-module $psakePath
-        Write-Output "[+] Loaded psake from $psakePath"
+    }
+}
+
+# Try to load a local copy first (so we can ensure a particular version is used)
+TryLoad-Psake .\lib\psake
+
+if ((get-module psake) -eq $null) {
+    # Don't have psake loaded, try to load it from PowerShell's default module location
+    import-module psake -ErrorAction SilentlyContinue
+}
+
+if ((get-module psake) -eq $null) {
+    # Not yet loaded, try to load it from the packages folder
+    TryLoad-Psake ".\packages\"
+}
+
+if ((get-module psake) -eq $null) {
+    # Still not loaded, let's try the various NuGet caches
+    $locals = .\nuget locals all -list
+    foreach($local in $locals)
+    {
+        $index = $local.IndexOf(":")
+        $folder = $local.Substring($index + 2)
+        TryLoad-Psake $folder
     }
 }
 
 $psake = get-module psake
 if ($psake -ne $null) {
     $psakePath = $psake.Path
-    Write-Output "[+] Psake already loaded from $psakePath"
+    Write-Output "[+] Psake loaded from $psakePath"
 }
-
-# Try to load a local copy first (so we can ensure a particular version is used)
-TryLoad-Psake .\lib\psake
-
-if ($psakeModule -eq $null) {
-    # Don't have psake not loaded, try to load it from PowerShell's default module location
-    import-module psake -ErrorAction SilentlyContinue
-}
-
-if ($env:USERPROFILE -ne $null) {
-    # Still don't  have psake loaded, try to load it from the Nuget cache in the users profile folder on Windows
-    TryLoad-Psake "$env:USERPROFILE\.nuget\psake\*"
-}
-
-if ($env:HOME -ne $null) {
-    # Still don't  have psake loaded, try to load it from the users profile folder on Linux
-    TryLoad-Psake "$env:HOME\.nuget\packages\psake\*\tools"
-}
-
-if ($env:NugetMachineInstallRoot-ne $null) {
-    # Still don't  have psake loaded, try to load it from the NuGet machine cache on Windows
-    TryLoad-Psake "$env:NugetMachineInstallRoot\.nuget\psake\*\psake.psm1"
-}
-
-$psakeModule = get-module psake
-if ($psakeModule -eq $null) {
+else {
     throw "Unable to load psake"
 }
 
